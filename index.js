e23815843cec122de6dd9626916beafc881ec81c
somme <sommesak@gmail.com>
2023-12-20 13:38:14 +0000

    updated case studies

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ var allenRoad = require('./routes/allenRoad');
 var ecards = require('./routes/ecards');
 var email = require('./routes/email');
 var appvia = require('./routes/appvia');
+var govsearch = require('./routes/govsearch');
+var faas = require('./routes/faas');
 
 //enviroment
 var env = process.env.NODE_ENV || 'development';
@@ -34,6 +36,8 @@ app.use('/allen-road', allenRoad);
 app.use('/marie-curie-ecards', ecards);
 app.use('/email', email);
 app.use('/casestudy/appvia', appvia);
+app.use('/casestudy/govsearch', govsearch);
+app.use('/casestudy/faas', faas);
 app.use(robots(__dirname + '/robots.txt'));
 
 
