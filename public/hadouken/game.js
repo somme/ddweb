c58a9d449ba4c57c0e2d0e45c6970e8e4ae7b7b7
somme <sommesak@gmail.com>
2023-12-21 12:21:03 +0000

    fixed game

--- public/hadouken/game.js
+++ public/hadouken/game.js
@@ -1,339 +1,155 @@
 $(document).ready(function() {
   //start crafty
-  Crafty.init(30, 1000, 400);
-  Crafty.canvas();
+  Crafty.init(1000, 400, document.getElementById('game'));
 
+  var fire = true;
 
-__CONFIG = {};
-__CONFIG.images = {
-  player: "hadouken/sprites/guy.png",
-  bground: "hadouken/sprites/bg.png",
-    box: "hadouken/sprites/box2.png"
+  var assetsObj = {
+      sprites: {
+        background: "hadouken/sprites/bg.png",
+        player: "hadouken/sprites/guy.png",
+        box: "hadouken/sprites/box2.png"
+      }
   };
 
-
-
-
+  //background sprites
+  Crafty.sprite(284, assetsObj.sprites.background, {
+    background1: [0,0],
+    background2: [1,0],
+    background3: [2,0],
+    background4: [3,0]
+  });
 
   //player sprites
-    Crafty.sprite(128, __CONFIG.images.player,
-    {
+  Crafty.sprite(128, assetsObj.sprites.player, {
     player: [0,0],
-        playerHadooken: [0,1],
-        player2: [0,2],
-        player2Hadooken: [0,3]
-
+    playerHadooken: [0,1],
   });
-    //background sprites
-    Crafty.sprite(284, __CONFIG.images.bground,
-    {
-    //bground: [0,0],
-    bground1: [0,0],
-        bground2: [1,0],
-        bground3: [2,0],
-        bground4: [3,0]
-    });
-     //box sprites
-    Crafty.sprite(124, __CONFIG.images.box,
-    {
-        box: [0,0]
-    });
+
+  //box sprites
+  Crafty.sprite(124, assetsObj.sprites.box, {
+     box: [0,0]
+  });
+
+    function boxes(x){
+      Crafty.e("2D, Canvas, box,  Animate, Gravity, Collision, SpriteAnimation, Tween")
+        .attr({x: x, y: 0, z: 1})
+        .gravity('Floor')
+        .onHit("playerHadooken", function() {
+          fire = true
+          Crafty.audio.play("hitsound");
+          playerHadooken.destroy();
+
+          this.reel("destroy_box", 100, 1, 0, 3)
+            .animate("destroy_box", 1)
+            .bind('AnimationEnd', function(){
+              this.destroy()
+              boxes(Math.floor(Math.random() * 600) + 200)
+            });
 
 
-
-
-  //add sounds
-Crafty.audio.add("hadookensound", ["hadouken/sounds/hadoken.wav"]).settings({volume: 0.8});
-Crafty.audio.add("hitsound", ["hadouken/sounds/hit.wav"]).settings({volume: 0.2});
-Crafty.audio.add("jumpsound", ["hadouken/sounds/jump.wav"]).settings({volume: 0.2});
-
-
-
-Crafty.c("health",{
-     _mana:100,
-     health:function(mana){
-       this._mana=mana;
-       return this
-       },
-       hurt:function(by){
-         this._mana-=by;
-         this.trigger("hurt",
-                {by:by,
-                mana:this._mana
-                });
-         if(this._mana<=0)this.trigger("die");
-         return this},
-         heal:function(by){
-           this._mana+=by;
-           this.trigger("heal");
-           return this
-           }
-           }
-);
-
-
-
- init: function generateWorld() {
-
-
-    //walls
-    for(var i = 1; i < 19; i++) {
-      Crafty.e("2D, DOM, wall_left, bush"+Crafty.randRange(1,2))
-        .attr({x: 0, y: i * 16, z: 2});
-      Crafty.e("2D, Canvas, wall_right, bush"+Crafty.randRange(1,2))
-        .attr({x: 1000, y: i * 16, z: 2});
+      })
     }
-  }
 
+   function addAssets() {
+     Crafty.audio.add('jumpsound', 'hadouken/sounds/jump.wav');
+     Crafty.audio.add("hadookensound", "hadouken/sounds/hadoken.wav");
+     Crafty.audio.add("hitsound", "hadouken/sounds/hit.wav");
+     Crafty.background("#041c63");
 
-  //the loading screen that will display while our assets load
-  Crafty.scene("loading", function() {
-    //load takes an array of assets and a callback when complete
-    Crafty.load(["hadouken/sprites/guy.png","hadouken/sprites/bg.png", "hadouken/sprites/box2.png"], function() {
-      Crafty.scene("main"); //when everything is loaded, run the main scene
-    });
+     //randomize background
+     for(var i = 0; i < 4; i++) {
+       var bg = Crafty.math.randomInt(1, 4);
+       Crafty.e("2D, Canvas, background"+bg).attr({x: i * 284, y:120, xspeed: 20});
+     }
 
-    //Transparent background with some loading text
-    Crafty.background("#041c63");
+     Crafty.e("2D, Canvas, wall_left").attr({x: 0, y: 0, w: 10, h: 350});
+     Crafty.e("2D, Canvas, wall_right").attr({x: 990, y: 0, w: 10, h: 350});
+     Crafty.e('Floor, 2D, Canvas').attr({x: 0, y: 350, w: 1000, h: 10})
 
-    Crafty.e("2D, DOM, Text").attr({w: 100, h: 20, x: 420, y: 150})
-      .text("Loading")
-      .css({"text-align": "center", "color":"#fff", 'font-size':'2em', 'margin':'0 auto'});
-  });
+      player = Crafty.e("2D, DOM, player, Twoway, Gravity, Collision, SpriteAnimation, Keyboard")
+      .attr({x: 0, y: 0, z: 1})
+      .twoway(200)
+      .gravity('Floor')
+      .reel('walk_left',   600, 10, 0, 10)
+      .animate("walk_left", 0)
+      .reel('walk_right', 600, 0, 0, 10)
+      .animate("walk_right", 0)
+      .reel('jump_up', 600,  3, 0, 3)
+      .animate("jump_up", 0)
+      .bind("KeyDown", function(e) {
+       if (e.key == Crafty.keys.LEFT_ARROW) {
+            this.animate("walk_left", -1);
+        } else if (e.key == Crafty.keys.RIGHT_ARROW) {
+          this.animate("walk_right", -1);
+        } else if (e.key == Crafty.keys.UP_ARROW) {
+          Crafty.audio.play("jumpsound");
+          this.animate("jump_up", 10);
+        } else if(e.key == Crafty.keys.F) {
 
-  //automatically play the loading scene
-  Crafty.scene("loading");
-
-  Crafty.scene("main", function() {
-    generateWorld();
-
-    Crafty.c('twoway', {
-      __move: {left:false,right:false,up:false,falling:false},
-      _speed: 10,
-
-      twoway: function(speed,jump) {
-        //if(speed) this._speed = speed*4;
-           if(speed) this._speed=speed;
-            //this._speed*4;
-        var move = this.__move;
-
-
-
-
-        this.bind('enterframe', function() {
-          //move the player in a direction depending on the booleans
-          //only move the player in one direction at a time (up/down/left/right)
-          if(this.isDown("RIGHT_ARROW")) this.x += this._speed;
-          else if(this.isDown("LEFT_ARROW")) this.x -= this._speed;
-          else if(this.isDown("UP_ARROW")) this.y -= this._speed;
-                    //else if(this.isDown("UP_ARROW")) this.y-=jump;this._falling=true;changed=true;
-                   // else if(move.up){this.y-=jump;this._falling=true;changed=true}
-          //else if(this.isDown("Z")) this.y += this._speed;
-                    else if(this.isDown("Z"));
-
-
-
-
-        });
-
-        return this;
-      }
-    });
-
-
-          //create an invisible floor entity
-        var floor = Crafty.e("2D, floor")
-        .attr({x:0, y: 342, w: 1000, h: 5});
-
-
-       Crafty.c('thrust', {
-      thrust: function() {
-        this.disabledControls = true;
-        this.bind('enterframe', function() {
-                //scroll player
-                         // this.x -= 4;
-
-
-          if(this.isDown('UP_ARROW')) {
-                        this.x += 4;
-                       // this.speed = 400;
+          function resetFire() {
+            fire = true
           }
 
-        });
-        return this;
+          if (!fire){
+            setTimeout(resetFire, 2000);
+          }
+
+        if(fire){
+            fire = false
+            Crafty.audio.play("hadookensound");
+            player.reel("hadooken",  100, [[20, 0], [21, 0], [22, 0]])
+              .animate("hadooken", 1);
+
+            //create a fireball entity
+            playerHadooken = Crafty.e("2D, DOM, playerHadooken, SpriteAnimation, Animate, Tween, Collision, audio")
+              .reel("fireball", 800, 0, 1, 7)
+              .animate("fireball", -1)
+              .attr({
+                x: player._x,
+                y: player._y,
+                w: 128,
+                h: 128,
+              })
+              .animate("fireball", -1)
+              .tween({x: 1000, y: this._y}, 2000)
+
+        }
+
       }
-    });
+  })
+  .bind("KeyUp", function(e) {
 
-        Crafty.c('thrustfireball', {
-      thrust: function() {
-        this.disabledControls = true;
-        this.bind('enterframe', function() {
-          if(this.isDown('Z')) {
-                        this.y = 0;
-                        this.x = 0;
-                        this.y += 300;
-                        this.speed = 1;
-                    }
-
-        });
-        return this;
-      }
-    });
-
-    Crafty.c('score', {
-      _score:0,
-      score: function(initial) {
-        this._score = initial;
-        return this;
-      },
-      addScore: function(points) {
-        this._score += points;
-        return this;
-      }
-    });
-
-
-    //create our player entity with some premade components
-    player = Crafty.e("2D, Canvas, player, Controls, twoway, Animate, Collision, gravity, thrust, score")
-      .attr({x: 0, y: 0, z: 1})
-      //.twoway(0)
-            .gravity('floor',20)
-            .twoway(10)
-            .thrust(200)
-      .animate("walk_left", 10, 0, 19)
-      .animate("walk_right", 0, 0, 9)
-      .animate("jump_up", 3, 0, 3)
-      .animate("hadooken", 20, 0, 22)
-
-        //player.animate("walk_right", 20, 10)
-      //.bind("enterframe", function(e) {
-
-
-
-      .bind("keydown", function(e) {
-
-
-                if(e.keyCode === Crafty.keys.RIGHT_ARROW) {
-console.log(  this.move)
-                    this.move.right = false;
-                    if(!this.isPlaying("walk_right"))
-                    this.stop().animate("walk_right", 20);
-
-        } else if(e.keyCode === Crafty.keys.LEFT_ARROW) {
-
-                    this.move.left = false;
-                    if(!this.isPlaying("walk_left"))
-                    this.stop().animate("walk_left", 20);
-
-        } else if(e.keyCode === Crafty.keys.UP_ARROW) {
-          //Crafty.audio.play("jumpsound");
-                    this.move.up = false;
-                    if(!this.isPlaying("jump_up"))
-                    Crafty.audio.play("jumpsound");
-                    this.stop().animate("jump_up", 10);
-
-        } else if(e.keyCode === Crafty.keys.Z) {
-
-
-                //create a fireball entity
-        playerHadooken = Crafty.e("2D, Canvas, playerHadooken, Animate, tween, Collision, audio")
-                playerHadooken.animate("fireball", 0, 1, 7)
-                playerHadooken.collision()
-        playerHadooken.onHit("box", function() {
-          player.addScore(Math.floor(Math.random()*4)+1);
-          $('#score').html(player._score);
-            Crafty.audio.play("hitsound");
-            this.destroy();
-            //box.destroy();
-
-            })
-
-            playerHadooken.attr({
-              x: this._x,
-              y: this._y,
-              w: 128,
-              h: 128,
-            })
-
-                       playerHadooken.animate("fireball", 100);
-                       playerHadooken.tween({x: 1000, y: this._y}, 100);
-                      Crafty.audio.play("hadookensound");
-
-                       this.stop().animate("hadooken", 6);
-                     //   playerHadooken.onHit("player2");
-
-
-
-                }
-
-
-      })
-
-
-
-      .collision()
-
-      .onHit("wall_left", function() {
-        this.x += this._speed;
-        //this.stop();
-      })
-            .onHit("wall_right", function() {
-        this.x -= this._speed;
-        //this.stop();
-      })
-      /*.onHit("box", function() {
-        this.x -= this._speed;
-      })*/
-
-
-
-
-            //player 2
-           /* player2 = Crafty.e("2D, Canvas, player2, Animate, gravity,collision")
-                .attr({x: 850, y: 0, z: 1})
-                .gravity('floor',20)*/
-
-      //box
-      var __x =0;
-      for (var i=0; i<4; i++) {
-        __x = Math.floor(Math.random() * 600) + 200 + __x;
-        box = Crafty.e("2D, Canvas, box, Animate, gravity,collision")
-                .attr({x: __x, y: 0, z: 1})
-                .gravity('floor',20)
-        .onHit("playerHadooken", function() {
-          this.destroy();
-          playerHadooken.destroy();
-                      Crafty.audio.play("hitsound");
-        })
-      }
-
-      //background
- /*         bground = Crafty.e("2D, Canvas, bground, Animate, tween")
-                bground.attr({x: 0, y: 120, z: 0})
-                //bground.animate("bground", 0, 0, 1)
-
-
-                                      .tween({x: -1000, y: 120}, 10000);
-
- */
-
-
-
-      for(var i = 0; i < 4; i++) {
-            bground = Crafty.randRange(1, 4);
-        Crafty.e("2D, Canvas, thrust, bground"+bground)
-          .attr({x: i * 284, y:120, xspeed: 20});
-
+    if (e.key == Crafty.keys.LEFT_ARROW) {
+      player.pauseAnimation()
+    }
+    if (e.key == Crafty.keys.RIGHT_ARROW) {
+      player.pauseAnimation()
+    }
+    if (e.key == Crafty.keys.UP_ARROW) {
+      player.pauseAnimation();
     }
 
-
-
-
-
-
-
-
+  })
+  .collision()
+  .onHit("wall_left", function() {
+    player.x = 10;
+  })
+  .onHit("wall_right", function() {
+    player.x = -10;
   });
 
+  boxes(Math.floor(Math.random() * 600) + 200)
+  }
+
+   Crafty.scene("main", function() {
+    Crafty.scene("main");
+   });
+
+  Crafty.load(assetsObj,
+    function() {
+      addAssets();
+    }
+  );
 
 });
